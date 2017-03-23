ed48096ccdeb13b91acb329108904b7e434c8778
crissdua <crissdua@hotmail.com>
2017-03-23 16:26:49 -0600

    Conexion SAU
    Select,Add,Update

---
+++ OASP_C/Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Leer"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(206, 34)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(206, 60)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(206, 86)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(206, 112)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 4
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(206, 138)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(107, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Cod Almacen"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(107, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Nom Almacen"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(107, 93)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Cod Item"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(107, 119)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Desc Item"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(107, 145)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(81, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Cant Disponible"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 83)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Enviar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 135)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Actualizar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(356, 198)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ OASP_C/Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim objetoLeer As New WSDL_BODEGAS.ReadResponse
+            objetoLeer.IF_BodegaReadByIDResponse_sync = New WSDL_BODEGAS.IF_BodegaReadByIDResponseMessage_sync
+            Dim cliente As New WSDL_BODEGAS.Y640YVQUY_WBodegaClient
+            cliente.ClientCredentials.UserName.UserName = "_BODEGAS"
+            cliente.ClientCredentials.UserName.Password = "Welcome08"
+            Dim leerrequest As New WSDL_BODEGAS.ReadRequest
+            cliente.Open()
+            leerrequest.IF_BodegaReadByIDQuery_sync = New WSDL_BODEGAS.IF_BodegaReadByIDQueryMessage_sync
+            leerrequest.IF_BodegaReadByIDQuery_sync.IF_Bodega = New WSDL_BODEGAS.IF_BodegaReadByIDQuery
+            leerrequest.IF_BodegaReadByIDQuery_sync.IF_Bodega.CodAlmacen = TextBox1.Text
+            objetoLeer.IF_BodegaReadByIDResponse_sync = New WSDL_BODEGAS.IF_BodegaReadByIDResponseMessage_sync
+            objetoLeer.IF_BodegaReadByIDResponse_sync = cliente.Read(leerrequest.IF_BodegaReadByIDQuery_sync)
+            TextBox2.Text = objetoLeer.IF_BodegaReadByIDResponse_sync.IF_Bodega.Nalmacen.Value.ToString
+            TextBox3.Text = objetoLeer.IF_BodegaReadByIDResponse_sync.IF_Bodega.Citem.Value.ToString
+            TextBox4.Text = objetoLeer.IF_BodegaReadByIDResponse_sync.IF_Bodega.Ditem.Value.ToString
+            TextBox5.Text = objetoLeer.IF_BodegaReadByIDResponse_sync.IF_Bodega.CantDisp.Value.ToString
+            ' MessageBox.Show(ajjj.IF_BodegaReadByIDResponse_sync.IF_Bodega.SAP_UUID.ToString)
+            cliente.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim objetoEscribir As New WSDL_BODEGAS.CreateResponse
+            objetoEscribir.IF_BodegaCreateConfirmation_sync = New WSDL_BODEGAS.IF_BodegaCreateConfirmationMessage_sync
+            Dim cliente As New WSDL_BODEGAS.Y640YVQUY_WBodegaClient
+            cliente.ClientCredentials.UserName.UserName = "_BODEGAS"
+            cliente.ClientCredentials.UserName.Password = "Welcome08"
+            Dim leerrequest As New WSDL_BODEGAS.CreateRequest
+            cliente.Open()
+            leerrequest.IF_BodegaCreateRequest_sync = New WSDL_BODEGAS.IF_BodegaCreateRequestMessage_sync
+            leerrequest.IF_BodegaCreateRequest_sync.IF_Bodega = New WSDL_BODEGAS.IF_BodegaCreateRequest
+            objetoEscribir.IF_BodegaCreateConfirmation_sync = New WSDL_BODEGAS.IF_BodegaCreateConfirmationMessage_sync
+            leerrequest.IF_BodegaCreateRequest_sync.IF_Bodega.CodAlmacen = TextBox1.Text
+            leerrequest.IF_BodegaCreateRequest_sync.IF_Bodega.Nalmacen = New WSDL_BODEGAS.Text
+            leerrequest.IF_BodegaCreateRequest_sync.IF_Bodega.Nalmacen.Value = TextBox2.Text
+            leerrequest.IF_BodegaCreateRequest_sync.IF_Bodega.Citem = New WSDL_BODEGAS.Text
+            leerrequest.IF_BodegaCreateRequest_sync.IF_Bodega.Citem.Value = TextBox3.Text
+            leerrequest.IF_BodegaCreateRequest_sync.IF_Bodega.Ditem = New WSDL_BODEGAS.Text
+            leerrequest.IF_BodegaCreateRequest_sync.IF_Bodega.Ditem.Value = TextBox4.Text
+            leerrequest.IF_BodegaCreateRequest_sync.IF_Bodega.CantDisp = New WSDL_BODEGAS.Quantity
+            leerrequest.IF_BodegaCreateRequest_sync.IF_Bodega.CantDisp.Value = TextBox5.Text
+            objetoEscribir.IF_BodegaCreateConfirmation_sync = cliente.Create(leerrequest.IF_BodegaCreateRequest_sync)
+            ' MessageBox.Show(ajjj.IF_BodegaReadByIDResponse_sync.IF_Bodega.SAP_UUID.ToString)
+            cliente.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim objetoActualizar As New WSDL_BODEGAS.UpdateResponse
+        objetoActualizar.IF_BodegaUpdateConfirmation_sync = New WSDL_BODEGAS.IF_BodegaUpdateConfirmationMessage_sync
+        Dim cliente As New WSDL_BODEGAS.Y640YVQUY_WBodegaClient
+        cliente.ClientCredentials.UserName.UserName = "_BODEGAS"
+        cliente.ClientCredentials.UserName.Password = "Welcome08"
+        Dim leerrequest As New WSDL_BODEGAS.UpdateRequest
+        cliente.Open()
+        leerrequest.IF_BodegaUpdateRequest_sync = New WSDL_BODEGAS.IF_BodegaUpdateRequestMessage_sync
+        leerrequest.IF_BodegaUpdateRequest_sync.IF_Bodega = New WSDL_BODEGAS.IF_BodegaUpdateRequest
+        objetoActualizar.IF_BodegaUpdateConfirmation_sync = New WSDL_BODEGAS.IF_BodegaUpdateConfirmationMessage_sync
+
+        leerrequest.IF_BodegaUpdateRequest_sync.IF_Bodega.CodAlmacen = TextBox1.Text
+        leerrequest.IF_BodegaUpdateRequest_sync.IF_Bodega.Nalmacen = New WSDL_BODEGAS.Text
+        leerrequest.IF_BodegaUpdateRequest_sync.IF_Bodega.Nalmacen.Value = TextBox2.Text
+        leerrequest.IF_BodegaUpdateRequest_sync.IF_Bodega.Citem = New WSDL_BODEGAS.Text
+        leerrequest.IF_BodegaUpdateRequest_sync.IF_Bodega.Citem.Value = TextBox3.Text
+        leerrequest.IF_BodegaUpdateRequest_sync.IF_Bodega.Ditem = New WSDL_BODEGAS.Text
+        leerrequest.IF_BodegaUpdateRequest_sync.IF_Bodega.Ditem.Value = TextBox4.Text
+        leerrequest.IF_BodegaUpdateRequest_sync.IF_Bodega.CantDisp = New WSDL_BODEGAS.Quantity
+        leerrequest.IF_BodegaUpdateRequest_sync.IF_Bodega.CantDisp.Value = TextBox5.Text
+        objetoActualizar.IF_BodegaUpdateConfirmation_sync = cliente.Update(leerrequest.IF_BodegaUpdateRequest_sync)
+        ' MessageBox.Show(ajjj.IF_BodegaReadByIDResponse_sync.IF_Bodega.SAP_UUID.ToString)
+        MessageBox.Show("Actualizo Correctamente")
+        cliente.Close()
+    End Sub
+End Class
